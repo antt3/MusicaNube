875ea0b94932154c6125393d3e02e8006e2efee7
Anthony T <anthonyt3@sbcglobal.net>
2022-06-25 12:02:04 -0700

    Added demoSongs seeder

---
+++ backend/db/seeders/20220625185115-demoSongs.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) {
+        return queryInterface.bulkInsert(
+	  	  	  'Songs',
+	  	  	  [
+	  	  	  	  {
+	  	  	  	  	  title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+	  	  	  	  	  userId: 1,
+	  	  	  	  	  createdAt: new Date(),
+	  	  	  	  	  updatedAt: new Date(),
+	  	  	  	  },
+                {
+                    title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+                    userId: 2,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+                    userId: 3,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+                    userId: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+                    userId: 2,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+	  	  	  	  	  title: '',
+                    artist: '',
+                    songPic: '',
+                    link: '',
+	  	  	  	  	  userId: 3,
+	  	  	  	  	  createdAt: new Date(),
+	  	  	  	  	  updatedAt: new Date(),
+	  	  	  	  },
+            ], {}
+        );
+    },
+    
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Songs', null, {});
+    },
+};
