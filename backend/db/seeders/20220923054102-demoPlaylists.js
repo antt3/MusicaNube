ec521e35927495d72672aa6d56aced26f06fb124
Anthony T <anthonyt3@sbcglobal.net>
2022-09-29 22:59:31 -0700

    Playlist/Song Seeder Additions & Minor Single Song CSS Addition

--- backend/db/seeders/20220923054102-demoPlaylists.js
+++ backend/db/seeders/20220923054102-demoPlaylists.js
@@ -8,14 +8,14 @@ module.exports = {
         {
           name: 'Playlist 1',
           userId: 1,
-          pic: "",
+          pic: "https://images.assetsdelivery.com/compings_v2/fokaspokas/fokaspokas1808/fokaspokas180802038.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Playlist 2',
           userId: 1,
-          pic: "",
+          pic: "https://i.pinimg.com/originals/f8/65/d3/f865d3112022612c6875b4ab7ec54239.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -43,7 +43,7 @@ module.exports = {
         {
           name: 'Playlist Dos',
           userId: 2,
-          pic: "",
+          pic: "https://i.pinimg.com/736x/55/27/89/552789ccf1e4e919e17930976a5e62c9.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -57,14 +57,14 @@ module.exports = {
         {
           name: 'My Favorites',
           userId: 3,
-          pic: "",
+          pic: "https://www.kindpng.com/picc/m/111-1119487_transparent-music-notes-heart-clipart-music-with-hearts.png",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Vibes',
           userId: 3,
-          pic: "",
+          pic: "https://i.pinimg.com/474x/7c/d1/e8/7cd1e861c95da51f9f5d89283bca7bf8.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
